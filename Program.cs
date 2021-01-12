14bdd829837afdd74b3f08c4e9a9ec8bc84e9995
Markos Ayala <elmarkos23@gmail.com>
2021-01-12 12:29:34 -0500

    ok

--- Program.cs
+++ Program.cs
@@ -4,8 +4,12 @@ using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Net;
+using System.Net.Http;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using WinSCP;
 
 namespace FTPConnect
 {
@@ -16,10 +20,37 @@ namespace FTPConnect
 			try
 			{
 			
-				string carpetaArchivoLocal = @"D:\yanki.mp3";
+				string carpetaArchivoLocal = @"D:\";
 
-				string PathFTP = "rutaftp";
-				string NombreArchvio = "file.jpg";
+				string PathFTP = "ftp.grupodifare.com";
+				string NombreArchvio = "yanki.mp3";
+				string Usuario = "confiamed";
+				string Clave = "C0nf1@m3d*";
+
+
+
+
+				// Setup session options
+				SessionOptions sessionOptions = new SessionOptions
+				{
+					Protocol = Protocol.Sftp,
+					FtpSecure = FtpSecure.Explicit,
+					HostName = PathFTP,
+					UserName =Usuario,
+					Password = Clave,
+					PortNumber=22
+//					SshHostKeyFingerprint = "ssh-rsa 2048 xxxxxxxxxxx...="
+				};
+
+				using (Session session = new Session())
+				{
+					// Connect
+					session.Open(sessionOptions);
+
+					// Upload files
+					session.PutFiles(carpetaArchivoLocal, NombreArchvio).Check();
+				}
+
 
 
 				// Get the object used to communicate with the server.
@@ -27,13 +58,19 @@ namespace FTPConnect
 				request.Method = WebRequestMethods.Ftp.UploadFile;
 
 				// This example assumes the FTP site uses anonymous logon.
-				request.Credentials = new NetworkCredential("mayala", "mayala2019");
+				request.Credentials = new NetworkCredential(Usuario, Clave);
 
 				// Copy the contents of the file to the request stream.
 				StreamReader sourceStream = new StreamReader(carpetaArchivoLocal);
 				byte[] fileContents = Encoding.UTF8.GetBytes(sourceStream.ReadToEnd());
 				sourceStream.Close();
 				request.ContentLength = fileContents.Length;
+				request.KeepAlive = false;
+				request.UseBinary = true;
+				request.Proxy = null;
+				request.UsePassive = false;
+				request.ClientCertificates = new X509CertificateCollection();
+				
 
 				Stream requestStream = request.GetRequestStream();
 				requestStream.Write(fileContents, 0, fileContents.Length);
